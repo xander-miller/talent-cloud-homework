e71e4dbb24c085ef82226b78c03d897a2190071d
Xander Miller <xander.miller@gmail.com>
2018-11-25 19:44:06 -0500

    carousel styled and working

--- prod/assets/js/carousel.js
+++ prod/assets/js/carousel.js
@@ -1,7 +1,19 @@
-$('.owl-carousel').owlCarousel({
+var owl = $('.owl-carousel').owlCarousel({
     items:1,
     loop:true,
     margin:10,
-    nav:true,
+    nav:false,
     dots:false,
+});
+
+owl.owlCarousel();
+// Go to the next item
+$('.customNextBtn').click(function() {
+    owl.trigger('next.owl.carousel');
+})
+// Go to the previous item
+$('.customPrevBtn').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel', [300]);
 })
